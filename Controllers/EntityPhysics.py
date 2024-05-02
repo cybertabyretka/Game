e9e196ee91bc7fd2d513d51e85e21c33fcf47c63
Vlad Kalshnikov <Kalashnikovv537@gmail.com>
2024-05-02 21:53:19 +0500

    Rebuild player physics (end)

--- Controllers/EntityPhysics.py
+++ Controllers/EntityPhysics.py
@@ -1,31 +1,22 @@
-from Models import Entities
-from abc import ABC
 import pygame as pg
 
 
-class EntityPhysics(ABC):
-    def update_pos(self):
-        pass
+class EntityPhysics:
+    def __init__(self, width, height, start_pos=(10., 10.), max_velocity=1):
+        self.collision = PlayerCollision(start_pos, (width, height))
+        self.max_velocity: float = max_velocity
+        self.velocity: list[float] = [0., 0.]
 
-    def attack(self):
-        pass
+    def update_collision(self):
+        self.collision.rect[0] += self.velocity[0]
+        self.collision.rect[1] += self.velocity[1]
 
 
 class PlayerCollision:
     def __init__(self, pos, size):
         self.rect = pg.Rect(pos[0], pos[1], size[0], size[1])
 
-    def update_collision(self, velocity):
-        self.rect[0] += velocity[0]
-        self.rect[1] += velocity[1]
-
 
 class PlayerPhysics(EntityPhysics):
-    def __init__(self, entity, start_pos=(10., 10.), max_velocity=1):
-        self.entity: Entities.Entity = entity
-        self.collision = PlayerCollision(start_pos, (entity.width, entity.height))
-        self.max_velocity: float = max_velocity
-        self.velocity: list[float] = [0., 0.]
-
-    def attack(self):
-        pass
+    def __init__(self, width, height, start_pos=(10., 10.), max_velocity=1):
+        super().__init__(width, height, start_pos, max_velocity)
--- Models/Entities.py
+++ Models/Entities.py
@@ -1,12 +1,14 @@
-import pygame as pg
+from Controllers.EntityPhysics import EntityPhysics, PlayerPhysics
 
 
 class Entity:
-    def __init__(self, width: float, height: float):
+    def __init__(self, width: float, height: float, start_pos=(350., 350.), max_velocity=1):
         self.width: float = width
         self.height: float = height
+        self.physic = EntityPhysics(width, height, start_pos, max_velocity)
 
 
 class Player(Entity):
-    def __init__(self, width=20, height=20):
+    def __init__(self, width=20., height=20., start_pos=(350., 350.), max_velocity=1):
         super().__init__(width, height)
+        self.physic = PlayerPhysics(width, height, start_pos, max_velocity)
